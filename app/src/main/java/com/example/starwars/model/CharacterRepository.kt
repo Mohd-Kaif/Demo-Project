e5b6b301cb1d2e8977cc13fed6ebb4b08d8c35ad
Mohd-Kaif <mkaif2196@gmail.com>
2025-03-17 13:49:20 +0530

    fixed error handling

--- app/src/main/java/com/example/starwars/model/CharacterRepository.kt
+++ app/src/main/java/com/example/starwars/model/CharacterRepository.kt
@@ -1,12 +1,17 @@
 package com.example.starwars.model
 
+import android.util.Log
 import com.example.starwars.data.CharacterData
 import com.example.starwars.network.StarWarsApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import com.example.starwars.viewModel.Result
+import com.example.starwars.viewModel.asResult
 import okio.IOException
 import org.json.JSONException
+import retrofit2.HttpException
+import java.net.SocketTimeoutException
 import javax.inject.Inject
 
 private const val TAG = "CharacterRepository"
@@ -17,15 +22,26 @@ class CharacterRepository @Inject constructor(
 //    private val api = RetrofitInstance.api
     private var nextPageUrl: String? = "https://swapi.dev/api/people/"
 
-    fun fetchAllCharacterData(): Flow<List<CharacterData>?> {
+    fun fetchAllCharacterData(): Flow<Result<List<CharacterData>>> {
         return flow {
+            if (nextPageUrl == null) {
+                return@flow
+            }
             val response = api.getCharacters(nextPageUrl!!)
+            Log.d(TAG, "${response.results.size}")
             emit(response.results)
             nextPageUrl = response.next
-            if (nextPageUrl == null) {
-                emit(null)
-            }
 //            Log.d(TAG, "Emitter Thread: ${Thread.currentThread()}")
-        }
+        }.asResult()
+            .catch { e->
+                Log.e(TAG, "${e.message}")
+                emit(when (e) {
+                    is SocketTimeoutException -> { Result.Error(Exception("Network Timeout. Please try again.")) }
+                    is IOException -> { Result.Error(Exception("No Internet Connection")) }
+                    is JSONException -> { Result.Error(Exception("Error parsing response."))}
+                    is HttpException -> { Result.Error(Exception("No Internet Connection.")) }
+                    else -> { Result.Error(e) }
+                })
+            }
     }
 }
--- app/src/main/java/com/example/starwars/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/starwars/viewModel/HomeViewModel.kt
@@ -53,36 +53,21 @@ class HomeViewModel @Inject constructor(
     fun getAllCharacterData() {
         if (!hasMoreData) return
         viewModelScope.launch(Dispatchers.IO) {
-//            if (hasNetwork() == false) return@launch
             characterRepository.fetchAllCharacterData()
                 .onStart { _isLoading.value = true }
                 .onCompletion { _isLoading.value = false }
-                .catch { e ->
-                    Log.e(TAG, "Error: ${e.message}")
-                    _homeUiState.value = when (e) {
-                        is SocketTimeoutException -> { Result.Error(Exception("Network Timeout. Please try again.")) }
-                        is IOException -> { Result.Error(Exception("No Internet Connection")) }
-                        is JSONException -> { Result.Error(Exception("Error parsing response."))}
-                        is HttpException -> {
-                            val errorMessage = when(e.code()) {
-                                400 -> "Bad Request. Please try again."
-                                401 -> "Unauthorized access. Check your credentials."
-                                404 -> "Not Found. The resource doesn't exist."
-                                500 -> "Server Error. Please try later."
-//                                Handle 504 error for Unsatisfiable Request (only-if-cached) -> Occurs when we try to load after cached data is loaded
-                                else -> "Unexpected Error: ${e.code()}"
-                            }
-                            Result.Error(Exception(errorMessage))
-                        }
-                        else -> { Result.Error(e) }
-                    }
-                }
                 .collect { result ->
                     val currentList = (_homeUiState.value as? Result.Success)?.data ?: emptyList()
-                    if (result == null) {
-                        hasMoreData = false
-                    } else {
-                        _homeUiState.value = Result.Success(currentList + result)
+                    when (result) {
+                        is Result.Success -> {
+                            _homeUiState.value = Result.Success(currentList + result.data)
+                        }
+                        is Result.Error -> {
+                            if (currentList.isEmpty()) {
+                                _homeUiState.value = Result.Error(result.exception)
+                            }
+                        }
+                        is Result.Loading -> {}
                     }
                 }
         }
@@ -93,4 +78,9 @@ sealed interface Result<out T> {
     data class Success<T>(val data: T) : Result<T>
     data class Error(val exception: Throwable?) : Result<Nothing>
     object Loading: Result<Nothing>
+}
+
+fun <T> Flow<T>.asResult(): Flow<Result<T>> {
+    return this
+        .map<T, Result<T>> { Result.Success(it) }
 }
