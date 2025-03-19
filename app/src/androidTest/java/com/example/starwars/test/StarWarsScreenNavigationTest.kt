caccd2033245d2f08a1b91979e9aa2427d3d554d
Mohd-Kaif <mkaif2196@gmail.com>
2025-03-19 13:00:37 +0530

    navigation test

--- app/src/androidTest/java/com/example/starwars/test/StarWarsScreenNavigationTest.kt
+++ app/src/androidTest/java/com/example/starwars/test/StarWarsScreenNavigationTest.kt
@@ -1,16 +1,28 @@
 package com.example.starwars.test
 
+import android.content.Context
+import android.content.Intent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.isRoot
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.printToLog
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.testing.TestNavHostController
 import com.example.starwars.MainActivity
+import com.example.starwars.R
 import com.example.starwars.StarWarsApp
+import com.example.starwars.data.CharacterData
 import com.example.starwars.view.navigation.StarWarsScreen
+import com.example.starwars.view.screens.CharacterDetailScreen
+import com.example.starwars.view.screens.HomeScreen
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Assert.assertEquals
@@ -27,7 +39,31 @@ class StarWarsScreenNavigationTest {
     @get:Rule(order = 2)
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
-    private lateinit var navController: TestNavHostController
+    private lateinit var navController: NavHostController
+
+    private fun shareDetails(context: Context, characterData: CharacterData?) {
+        if (characterData == null) return
+        val shareText =
+            """
+            |Name: ${characterData.name}
+            |Height: ${characterData.height}cm
+            |Gender: ${characterData.gender}
+            |Birth Year: ${characterData.birth_year}
+            |Eye Color: ${characterData.eye_color}
+            |Hair Color: ${characterData.hair_color}
+        """.trimMargin()
+        val shareIntent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/*"
+            putExtra(Intent.EXTRA_TEXT, shareText)
+        }
+
+        context.startActivity(
+            Intent.createChooser(
+                shareIntent,
+                context.getString(R.string.app_name)
+            )
+        )
+    }
 
     @Before
     fun setup() {
@@ -37,16 +73,37 @@ class StarWarsScreenNavigationTest {
     @Before
     fun setupStarWarsNavHost() {
         composeTestRule.activity.setContent {
-            navController = TestNavHostController(LocalContext.current).apply {
-                navigatorProvider.addNavigator(ComposeNavigator())
-            }
-            StarWarsApp(navController = navController)
-        }
-    }
+            navController = rememberNavController()
 
-    @Test
-    fun starWarsNavHost_verifyStartDestination() {
-        assertEquals(StarWarsScreen.Home.name , navController.currentBackStackEntry?.destination?.route)
+            NavHost(
+                navController = navController,
+                startDestination = StarWarsScreen.Home.name,
+            ) {
+                composable(route = StarWarsScreen.Home.name) {
+                    val context = LocalContext.current
+                    HomeScreen(
+                        navigateToCharacterDetails = { character ->
+                            navController.currentBackStackEntry?.savedStateHandle?.set(context.getString(
+                                R.string.character), character)
+                            navController.navigate(StarWarsScreen.CharacterDetails.name)
+                        }
+                    )
+                }
+
+                composable(route = StarWarsScreen.CharacterDetails.name) {
+                    val context = LocalContext.current
+                    val character = navController.previousBackStackEntry?.savedStateHandle?.get<CharacterData>(context.getString(
+                        R.string.character))
+                    if (character != null) {
+                        CharacterDetailScreen(
+                            character = character,
+                            navigateBack = { navController.navigateUp() },
+                            shareDetails = { shareDetails(context, character) }
+                        )
+                    }
+                }
+            }
+        }
     }
 
     @Test
@@ -54,11 +111,8 @@ class StarWarsScreenNavigationTest {
         composeTestRule.onNodeWithContentDescription("Back").assertDoesNotExist()
     }
 
-    // mock data required
-//    @Test
-//    fun starWarsNavHost_clickOneCharacter_navigatesToCharacterDetailsScreen() {
-//        composeTestRule.onNodeWithContentDescription("character")
-//            .performClick()
-//        assertEquals(StarWarsScreen.CharacterDetails.name, navController.currentBackStackEntry?.destination?.route)
-//    }
+    @Test
+    fun starWarsNavHost_verifyShareNavigationNotShownOnHomeScreen() {
+        composeTestRule.onNodeWithContentDescription("Share").assertDoesNotExist()
+    }
 }
